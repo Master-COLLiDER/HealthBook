bb4397eec9559526b162af73184332ebf15b8d06
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2020-08-28 16:47:37 +0530

    New Pages and features added

--- app/src/main/java/com/telescopeapplications/healthbook/SlideActivity2.java
+++ app/src/main/java/com/telescopeapplications/healthbook/SlideActivity2.java
@@ -2,7 +2,9 @@ package com.telescopeapplications.healthbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SlideActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SlideActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide2);
+
+        getWindow().getDecorView().findViewById(R.id.toolbar7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
