9e35e5327907dd4aaca78e50d51aa74414839038
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2020-08-28 18:58:05 +0530

    New "NUO selector  bug fixed"

--- app/src/main/java/com/telescopeapplications/healthbook/NUOselector.java
+++ app/src/main/java/com/telescopeapplications/healthbook/NUOselector.java
@@ -46,7 +46,7 @@ public class NUOselector extends AppCompatActivity {
                 if(T.equals("Veg"))
                 {
 
-                        openoverweight();
+                        openoverweightVeg();
                     // System.out.println("Collider: "+T);
 
                 }
@@ -61,7 +61,7 @@ public class NUOselector extends AppCompatActivity {
 
                  if(T.equals("Veg"))
                  {
-                        openobese();
+                        openobeseVeg();
                     // System.out.println("Collider: "+T);
 
                 }
