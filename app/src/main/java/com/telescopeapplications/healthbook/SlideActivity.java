e417974f36fe516d75ead7dc0f6491b8492f3b94
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2020-08-28 18:55:32 +0530

    Revert "NUO selector  bug fixed 2"
    This reverts commit 0af85ba5

--- app/src/main/java/com/telescopeapplications/healthbook/SlideActivity.java
+++ app/src/main/java/com/telescopeapplications/healthbook/SlideActivity.java
@@ -2,13 +2,47 @@ package com.telescopeapplications.healthbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class SlideActivity extends AppCompatActivity {
-
+    boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide);
+
+        getWindow().getDecorView().findViewById(R.id.toolbar5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), SlideActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
