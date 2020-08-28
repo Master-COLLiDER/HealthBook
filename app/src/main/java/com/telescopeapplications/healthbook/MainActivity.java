7e73620632d24a07079c6a1f26668d1aa72fa707
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2020-08-28 14:06:12 +0530

    Press two times back in main activity to exit

--- app/src/main/java/com/telescopeapplications/healthbook/MainActivity.java
+++ app/src/main/java/com/telescopeapplications/healthbook/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton BtnMove;
     private ImageButton BtnMove2;
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +41,23 @@ public class MainActivity extends AppCompatActivity {
     private void movetosecond() {
         Intent intent = new Intent(MainActivity.this, bmi.class);
         startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
