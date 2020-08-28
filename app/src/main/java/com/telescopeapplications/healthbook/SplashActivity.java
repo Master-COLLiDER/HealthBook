beb41161525c60f0b7e8198a2e170dc20e2bf91d
Master-COLLiDER <probaldeepsaikia@hotmail.com>
2020-08-28 13:43:59 +0530

    clipboardPatchFile

---
+++ app/src/main/java/com/telescopeapplications/healthbook/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.telescopeapplications.healthbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.telescopeapplications.healthbook.MainActivity;
+import com.telescopeapplications.healthbook.R;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2500);
+                    Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+}
